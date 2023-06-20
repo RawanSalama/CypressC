424eef2f1152774072a92ec47eb3d0fb89d04d04
RawanSalama <rawan@userpilot.co>
2023-06-20 15:27:51 +0300

    change the selector for publish

--- cypress/e2e/userpilot/userpilot.cy.js
+++ cypress/e2e/userpilot/userpilot.cy.js
@@ -1,6 +1,6 @@
 import { When, Then, Given } from "@badeball/cypress-cucumber-preprocessor";
-let PageEffectiveness = "#table-container-id";
-let UserActivity = "#table-container-id";
+let publish =
+  "BUTTON.MuiButtonBase-root.MuiButton-root.MuiButton-text.MuiButton-disableElevation[tabindex='0'][type='button']:nth-child(2) > span.MuiButton-label";
 const users = require("./users");
 Given("The user visits Userpilot Website", () => {
   cy.visit("https://nxtg-dev.userpilot.io/users");
@@ -21,16 +21,14 @@ When("The user clicks on User Feedback", () => {
 When("The user creates new survey and select template", () => {
   cy.contains("Surveys").click();
   cy.contains("Create Survey").click();
-  cy.contains("Evaluate The User Experience").trigger("mouseover");
+  cy.contains("Feature Research Survey").scrollIntoView().trigger("mouseover");
   cy.contains("Use Template").click({ force: true });
+  cy.wait(10000);
 });
 When("The user clicks on publish", () => {
   cy.contains("Publish").click();
-  cy.get(".MuiDialogActions-root > .jss1501 > .MuiButton-label").click({
-    force: true,
-  });
-  //cy.wait(10000);
 });
 Then("The user must recieved successful message", () => {
+  cy.get(publish).click();
   cy.contains("Survey published successfully").should("exist");
 });
